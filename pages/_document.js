ed99851211e55e88ec6087c569f6b4b1df8a95f8
Gaurav Tewari <gauravtewari111@gmail.com>
2022-04-17 20:07:47 +0530

    feat: analyatics added

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head >
+    <script async defer data-website-id="f9979386-ae24-4a54-81b6-2bbeff423585" src="https://umami-production-950e.up.railway.app/umami.js"></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
